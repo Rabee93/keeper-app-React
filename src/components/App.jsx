d9eb58df875a4dad444376f5945c736605d458c2
Rabee Mghrabi <rabeemghrabi93@gmail.com>
2021-10-25 15:09:30 +0100

    iterated over notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,12 +16,27 @@
 import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
+import Note from "./Note"
+import notes from "../notes"
+
+
+
 
 function App() {
   return (  <div>
    <Header />
+   {notes.map((n) => {
+    return (<Note
+    key = {n.key}
+    title = {n.title}
+    content = {n.content}
+    />
+  )
+  })}
    <Footer />
-  </div>
+
+ </div>
+
 )
 }
 

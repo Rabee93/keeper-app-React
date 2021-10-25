9846ef18ebc4aa4838645e4b243e02dfb50a7c14
Rabee Mghrabi <rabeemghrabi93@gmail.com>
2021-10-25 14:21:52 +0100

    added contacts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,15 @@ import React from "react"
 
 
 function Footer() {
-  const date = new Date()
-  const year = date.getFullYear()
+  const currentYear = new Date().getFullYear();
 
   return(
 <footer>
-    <p> Copy write by Rabee {year} </p>
+    <p> copyright @ {currentYear} </p>
   </footer>
 )
 
 }
 
-// test
+
 export default Footer;
